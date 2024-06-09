49920add9ddecbdd33af4ecd855ee26367423969
RichardPickman <drezzerock@gmail.com>
2024-06-09 19:35:04 +0200

    add stack creator

---
+++ scripts/stack.lib.ts
@@ -0,0 +1,64 @@
+import { CfnOutput, Stack, StackProps } from "aws-cdk-lib";
+import {
+  CloudFrontWebDistribution,
+  OriginAccessIdentity,
+} from "aws-cdk-lib/aws-cloudfront";
+import { CanonicalUserPrincipal, PolicyStatement } from "aws-cdk-lib/aws-iam";
+import { Bucket } from "aws-cdk-lib/aws-s3";
+import { BucketDeployment, Source } from "aws-cdk-lib/aws-s3-deployment";
+import { Construct } from "constructs";
+
+export class StaticSiteStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    const cloudfrontOAI = new OriginAccessIdentity(this, "MyStaticSiteOAI");
+
+    const bucket = Bucket.fromBucketArn(
+      this,
+      "ExistingBucket",
+      "arn:aws:s3:::rs-school-shop-bucket"
+    );
+
+    const bucketPolicy = new PolicyStatement({
+      actions: ["s3:GetObject"],
+      resources: [bucket.arnForObjects("*")],
+      principals: [
+        new CanonicalUserPrincipal(
+          cloudfrontOAI.cloudFrontOriginAccessIdentityS3CanonicalUserId
+        ),
+      ],
+    });
+
+    bucket.addToResourcePolicy(bucketPolicy);
+
+    const distribution = new CloudFrontWebDistribution(
+      this,
+      "MyStaticSiteDistribution",
+      {
+        originConfigs: [
+          {
+            s3OriginSource: {
+              s3BucketSource: bucket,
+              originAccessIdentity: cloudfrontOAI,
+            },
+            behaviors: [{ isDefaultBehavior: true }],
+          },
+        ],
+        defaultRootObject: "index.html",
+      }
+    );
+
+    new BucketDeployment(this, "MyStaticSiteDeployment", {
+      sources: [Source.asset("./dist")],
+      destinationBucket: bucket,
+      distribution,
+      distributionPaths: ["/*"],
+    });
+
+    new CfnOutput(this, "bucket", { value: bucket.bucketName });
+    new CfnOutput(this, "distribution", {
+      value: distribution.distributionDomainName,
+    });
+  }
+}
