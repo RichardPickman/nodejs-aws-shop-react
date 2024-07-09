019fdc25ecb5f86296c30aac7c27d06e81442528
Richard Pickman <drezzerock@gmail.com>
2024-07-09 19:43:02 +0200

    update links

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://qtz38gflp6.execute-api.eu-north-1.amazonaws.com/prod/",
+  import: "https://qtz38gflp6.execute-api.eu-north-1.amazonaws.com/prod",
   bff: "https://bqjc2zpho4.execute-api.eu-north-1.amazonaws.com/prod",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
