f1dfc075681eeab018f9326e017408b854c8ad9c
Richard Pickman <drezzerock@gmail.com>
2024-07-15 15:42:40 +0200

    add token puller from localStorage and set header for authorized requests

--- src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
+++ src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
@@ -26,12 +26,17 @@ export default function CSVFileImport({ url, title }: CSVFileImportProps) {
   const uploadFile = async () => {
     console.log("uploadFile to", url);
 
+    const token = localStorage.getItem("authorization_token");
+
     if (file) {
       const response = await axios({
         method: "GET",
         url,
         params: {
           name: encodeURIComponent(file.name),
+        },
+        headers: {
+          Authorization: `Basic ${token}`,
         },
       });
       console.log("File to upload: ", file.name);
