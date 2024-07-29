9a8a6ef3451981d6afc62471c95a08d7c053f754
Richard Pickman <drezzerock@gmail.com>
2024-07-29 15:29:26 +0200

    update api path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,10 +2,11 @@
 
 const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  order: "http://localhost:5000/api/profile/cart/checkout",
+  order:
+    "https://ncjsivfss1.execute-api.eu-west-2.amazonaws.com/prod/api/profile/cart/checkout",
   import: "https://vmeuwnvnaa.execute-api.eu-west-2.amazonaws.com/prod",
   bff: "https://gzog2ythal.execute-api.eu-west-2.amazonaws.com/prod",
-  cart: "http://localhost:5000/api",
+  cart: "https://ncjsivfss1.execute-api.eu-west-2.amazonaws.com/prod/api",
 };
 
 export default API_PATHS;
