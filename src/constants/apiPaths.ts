1436fbba2ed819150ae9ba1ff865d6f5c7e2db42
Richard Pickman <drezzerock@gmail.com>
2024-08-07 16:31:05 +0200

    add forgotten https

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -6,7 +6,7 @@ const API_PATHS = {
     "https://ncjsivfss1.execute-api.eu-west-2.amazonaws.com/prod/api/profile/cart/checkout",
   import: "https://vmeuwnvnaa.execute-api.eu-west-2.amazonaws.com/prod",
   bff: "https://gzog2ythal.execute-api.eu-west-2.amazonaws.com/prod",
-  cart: "rp-cart-api-development3.eba-wjwjazex.eu-west-3.elasticbeanstalk.com/api",
+  cart: "https://rp-cart-api-development3.eba-wjwjazex.eu-west-3.elasticbeanstalk.com/api",
 };
 
 export default API_PATHS;
