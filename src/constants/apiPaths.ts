70efd82b66e645489aa30948dae24b8a22d7e168
Richard Pickman <drezzerock@gmail.com>
2024-08-14 14:37:40 +0200

    update api paths

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -6,7 +6,7 @@ const API_PATHS = {
     "https://ncjsivfss1.execute-api.eu-west-2.amazonaws.com/prod/api/profile/cart/checkout",
   import: "https://vmeuwnvnaa.execute-api.eu-west-2.amazonaws.com/prod",
   bff: "http://eb-sucks-never-use-it.eba-u62vd4mb.eu-west-3.elasticbeanstalk.com",
-  cart: "http://eb-cart-sucks.eba-qmn4kekx.eu-west-3.elasticbeanstalk.com/api",
+  cart: "http://rp-task-10-cart-service.eba-jri33ieq.eu-west-3.elasticbeanstalk.com/api",
 };
 
 export default API_PATHS;
